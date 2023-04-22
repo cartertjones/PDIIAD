16dddfb89aced5bd55ff4bb149b2a8cb2c9ff82d
carterjones353 <117482851+carterjones353@users.noreply.github.com>
2023-04-22 02:22:08 -0600

    Finalized Divorce Interactivity
    Rewrote DivorceInteractivity.cs to be compatible with final activity design, now uses cropped images and layer sortingOrder.

--- Assets/Scripts/Interactivity/DivorceInteractivity.cs
+++ Assets/Scripts/Interactivity/DivorceInteractivity.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-
-
 public class DivorceInteractivity : MonoBehaviour
 {
     //[SerializeField] private UnityEngine.UI.Slider slider;
-    [SerializeField] private GameObject dadSquare;
-    [SerializeField] private GameObject momSquare;
+    //[SerializeField] private GameObject dadSquare;
+    //[SerializeField] private GameObject momSquare;
     [SerializeField] private Slider divorceSlider;
     private Material currentMatDad;
     private Material currentMatMom;
+    private Material currentMatBoth;
+
+    //TODO FIX ALPHA TRANSITION
+
+    //carter divorce change
+    [SerializeField] private GameObject momPanel;
+    [SerializeField] private GameObject dadPanel;
+    [SerializeField] private GameObject finalPanel;
+    private SpriteRenderer msr, dsr, fsr;
 
     private bool momSet = false;
     private bool dadSet = false;
@@ -28,34 +34,39 @@ public class DivorceInteractivity : MonoBehaviour
 
     void Start()
     {
-        currentMatDad = dadSquare.GetComponent<Renderer>().material;
-        currentMatMom = momSquare.GetComponent<Renderer>().material;
-        momSquare.gameObject.SetActive(false);
-        dadSquare.gameObject.SetActive(false);
+        msr = momPanel.GetComponent<SpriteRenderer>();
+        dsr = dadPanel.GetComponent<SpriteRenderer>();
+        fsr = finalPanel.GetComponent<SpriteRenderer>();
+
+        Reset();
     }
 
-     void OnSliderChanged(float alphaValue)
+     void OnSliderChanged(float changeAmount)
     {
-       if (alphaValue < 0f && !momSet)
+        if (divorceSlider.value < 0f && !momSet)
         {
             Color momOldColor = currentMatMom.color;
-            Color momSquare = new Color(momOldColor.r, momOldColor.g, momOldColor.b, (alphaValue ) +1.1f);
-            //Debug.Log("Slider" + (alphaValue));
-            //Debug.Log("Alpha" + (alphaValue + 1f));
+            Color momSquare = new Color(momOldColor.r, momOldColor.g, momOldColor.b, momOldColor.a + Mathf.Abs(changeAmount));
+
             currentMatMom.color = momSquare;
-            if (alphaValue <= -1f)
+
+            msr.sortingOrder = dsr.sortingOrder + 1;
+
+            if (divorceSlider.value <= -0.99f)
             {
                 momSet = true;
             }
-
         }
-        if (alphaValue > 0f && !dadSet)
+        if (divorceSlider.value > 0f && !dadSet)
         {
             Color dadOldColor = currentMatDad.color;
-            Color dadSquare = new Color(dadOldColor.r, dadOldColor.g, dadOldColor.b, (-alphaValue) +1.1f );
-            //Debug.Log("" + (alphaValue));
+            Color dadSquare = new Color(dadOldColor.r, dadOldColor.g, dadOldColor.b, dadOldColor.a + changeAmount);
+
             currentMatDad.color = dadSquare;
-            if (alphaValue >= 1f)
+
+            dsr.sortingOrder = msr.sortingOrder + 1;
+
+            if (divorceSlider.value >= 0.99f)
             {
                 dadSet = true;
             }
@@ -64,6 +75,8 @@ public class DivorceInteractivity : MonoBehaviour
         if(momSet && dadSet)
         {
             activityComplete = true;
+
+            fsr.sortingOrder++;
 
             //hide slider when activity complete
             divorceSlider.gameObject.SetActive(false);
@@ -78,19 +91,26 @@ public class DivorceInteractivity : MonoBehaviour
     public void Show()
     {
         divorceSlider.gameObject.SetActive(true);
-        momSquare.gameObject.SetActive(true);
-        dadSquare.gameObject.SetActive(true);
     }
     //hide all relevant gameobjects
     public void Hide()
     {
         divorceSlider.gameObject.SetActive(false);
-        momSquare.gameObject.SetActive(false);
-        dadSquare.gameObject.SetActive(false);
     }
     //reset activity progress
     public void Reset()
     {
         activityComplete = false;
+
+        currentMatMom = msr.material;
+        currentMatDad = dsr.material;
+        currentMatBoth = fsr.material;
+
+        msr.color = currentMatMom.color;
+        dsr.color = currentMatDad.color;
+        fsr.color = currentMatBoth.color;
+
+        currentMatMom.color = new Color(msr.color.r, msr.color.g, msr.color.b, 0);
+        currentMatDad.color = new Color(dsr.color.r, dsr.color.g, dsr.color.b, 0);
     }
 }
